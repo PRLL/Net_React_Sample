fc663c06354d473a22d6076fc27f518b8b6a953a
PRLL <josemoises.toyosvargas@hotmail.com>
2021-05-09 22:35:17 -0400

    fix: blob custom csp

--- API/Startup.cs
+++ API/Startup.cs
@@ -199,7 +199,7 @@ namespace API
                     "https://platform-lookaside.fbsbx.com",
                     "https://scontent-iad3-2.xx.fbcdn.net",
                     "https://scontent-iad3-1.xx.fbcdn.net",
-                    "blob:https://net-react-sample.herokuapp.com"
+                    "blob:"
                 ))
                 .ScriptSources(configuration => configuration.Self().CustomSources(
                     "sha256-ma5XxS1EBgt17N22Qq31rOxxRWRfzUTQS1KOtfYwuNo=",
